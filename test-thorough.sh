fb8059f91e195a0c958d2ca9edbe0118fe1875b5
Vaclav Petras <wenzeslaus@gmail.com>
2020-02-14 15:53:58 -0500

    Use full bin name and NC v2alpha
    We are not aliasing grass79 in any way, so no grass.
    Tests need the new NC SPM sample location (using v2alpha).

--- test-thorough.sh
+++ test-thorough.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/tcsh
 
-grass --tmp-location XY --exec \
+grass79 --tmp-location XY --exec \
     g.extension g.download.location
-grass --tmp-location XY --exec \
-    g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_08_grass7.tar.gz dbase=$HOME
+grass79 --tmp-location XY --exec \
+    g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha.tar.gz dbase=$HOME
 
-grass --tmp-location XY --exec \
-    python3 -m grass.gunittest.main --grassdata $HOME --location nc_spm_08_grass7 --location-type nc
+grass79 --tmp-location XY --exec \
+    python3 -m grass.gunittest.main --grassdata $HOME --location nc_spm_full_v2alpha --location-type nc
