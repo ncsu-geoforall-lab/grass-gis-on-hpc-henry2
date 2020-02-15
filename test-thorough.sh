440341a17de663f2c28ff2e2a4261483538b46c1
Vaclav Petras <wenzeslaus@gmail.com>
2020-02-14 23:31:39 -0500

    Use a new version of the sample location
    Old one has subdirs which current g.download.location cannot handle.

--- test-thorough.sh
+++ test-thorough.sh
@@ -3,7 +3,7 @@
 grass79 --tmp-location XY --exec \
     g.extension g.download.location
 grass79 --tmp-location XY --exec \
-    g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha.tar.gz dbase=$HOME
+    g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=$HOME
 
 grass79 --tmp-location XY --exec \
-    python3 -m grass.gunittest.main --grassdata $HOME --location nc_spm_full_v2alpha --location-type nc
+    python3 -m grass.gunittest.main --grassdata $HOME --location nc_spm_full_v2alpha2 --location-type nc
