86dae43270508d9350e0ac0f066e2127a7212f52
Vaclav Petras <wenzeslaus@gmail.com>
2020-04-17 09:34:03 -0400

    Specify required success rate for tests
    The test module now gives non-zero return code when certain percentage
    of tests fails. Setting it low enough for the test to succeed.

--- test-thorough.sh
+++ test-thorough.sh
@@ -6,4 +6,6 @@ grass79 --tmp-location XY --exec \
     g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=$HOME
 
 grass79 --tmp-location XY --exec \
-    python3 -m grass.gunittest.main --grassdata $HOME --location nc_spm_full_v2alpha2 --location-type nc
+    python3 -m grass.gunittest.main \
+        --grassdata $HOME --location nc_spm_full_v2alpha2 --location-type nc \
+        --min-success 50
