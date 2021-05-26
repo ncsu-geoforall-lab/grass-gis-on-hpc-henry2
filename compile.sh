b087e3b20e4170abfed0e4bc506633076a2bfcbf
Vaclav Petras <wenzeslaus@gmail.com>
2021-05-26 15:54:51 -0400

    Use conda and Bash everywhere

--- compile.sh
+++ compile.sh
@@ -1,90 +1,87 @@
 #!/usr/bin/bash
 
-# The make step requires something like:
-# setenv LD_LIBRARY_PATH $LD_LIBRARY_PATH\:$PREFIX/lib\:$PREFIX/lib64
-# further steps additionally require:
-# setenv PATH $PATH\:$PREFIX/bin
+# The make step requires something access to dynamically linked libraries:
+# LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 
-if ($#argv != 2) then
-    echo "Usage: $0 LIB_PREFIX INSTALL_PREFIX"
+if [[ $# -ne 3 ]] then
+    echo >&2 "Usage: $0 GRASS_VERSION LIB_PREFIX INSTALL_PREFIX"
     exit 1
-endif
+fi
 
-INSTALL_PREFIX=$argv[1]
-LIBS=
+GRASS_VERSION=$1
+LIBS=$2
+INSTALL_PREFIX=$3
 
-# GRASS GIS
+# Get the code
 
-git clone https://github.com/OSGeo/grass.git
+git clone --depth=1 --branch GRASS_VERSION https://github.com/OSGeo/grass.git grass-code
 
-cd grass
+cd grass-code
 
-setenv CFLAGS "-std=gnu99 -O0"
-setenv CXXFLAGS "-std=c++11 -O0"
+# Configure with dependencies
 
 ./configure \
-  --prefix=$INSTALL_PREFIX/ \
-  --with-openmp \
-  --with-pthread \
-  --with-freetype \
-  --with-freetype-includes=$LIBS/include/freetype2 \
-  --with-freetype-libs=$LIBS/lib \
-  --with-gdal=$LIBS/bin/gdal-config \
-  --with-gdal-libs=$LIBS/lib \
-  --with-proj=$LIBS/bin/proj \
-  --with-proj-includes=$LIBS/include \
-  --with-proj-libs=$LIBS/lib \
-  --with-proj-share=$LIBS/share/proj \
-  --with-geos=$LIBS/bin/geos-config \
-  --with-jpeg-includes=$LIBS/include \
-  --with-jpeg-libs=/$LIBS/lib \
-  --with-png-includes=$LIBS/include \
-  --with-png-libs=$LIBS/lib \
-  --with-tiff-includes=$LIBS/include \
-  --with-tiff-libs=$LIBS/lib \
-  --with-postgres=yes \
-  --with-postgres-includes=$LIBS/include \
-  --with-postgres-libs=$LIBS/lib \
-  --without-mysql \
-  --with-sqlite \
-  --with-sqlite-libs=$LIBS/lib \
-  --with-sqlite-includes=$LIBS/include \
-  --with-fftw-includes=$LIBS/include \
-  --with-fftw-libs=$LIBS/lib \
-  --with-cxx \
-  --with-cairo \
-  --with-cairo-includes=$LIBS/include/cairo \
-  --with-cairo-libs=$LIBS/lib \
-  --with-cairo-ldflags="-lcairo" \
-  --with-zstd \
-  --with-zstd-libs=$LIBS/lib \
-  --with-zstd-includes=$LIBS/include \
-  --with-bzlib \
-  --with-bzlib-libs=$LIBS/lib \
-  --with-bzlib-includes=$LIBS/include \
-  --with-netcdf=$LIBS/bin/nc-config \
-  --with-blas \
-  --with-blas-libs=$LIBS/lib \
-  --with-blas-includes=$LIBS/include \
-  --with-lapack \
-  --with-lapack-includes=$LIBS/include \
-  --with-lapack-libs=$LIBS/lib \
-  --with-netcdf=$LIBS/bin/nc-config \
-  --with-nls \
-  --with-libs=$LIBS/lib \
-  --with-includes=$LIBS/include \
-  --with-pdal=$LIBS/bin/pdal-config \
-  --with-pdal-libs=$LIBS/lib \
-  --with-readline \
-  --with-readline-includes=$LIBS/include/readline \
-  --with-readline-libs=$LIBS/lib
+    --prefix=$INSTALL_PREFIX/ \
+    --with-openmp \
+    --with-pthread \
+    --with-freetype \
+    --with-freetype-includes=$LIBS/include/freetype2 \
+    --with-freetype-libs=$LIBS/lib \
+    --with-gdal=$LIBS/bin/gdal-config \
+    --with-gdal-libs=$LIBS/lib \
+    --with-proj=$LIBS/bin/proj \
+    --with-proj-includes=$LIBS/include \
+    --with-proj-libs=$LIBS/lib \
+    --with-proj-share=$LIBS/share/proj \
+    --with-geos=$LIBS/bin/geos-config \
+    --with-jpeg-includes=$LIBS/include \
+    --with-jpeg-libs=/$LIBS/lib \
+    --with-png-includes=$LIBS/include \
+    --with-png-libs=$LIBS/lib \
+    --with-tiff-includes=$LIBS/include \
+    --with-tiff-libs=$LIBS/lib \
+    --with-postgres=yes \
+    --with-postgres-includes=$LIBS/include \
+    --with-postgres-libs=$LIBS/lib \
+    --without-mysql \
+    --with-sqlite \
+    --with-sqlite-libs=$LIBS/lib \
+    --with-sqlite-includes=$LIBS/include \
+    --with-fftw-includes=$LIBS/include \
+    --with-fftw-libs=$LIBS/lib \
+    --with-cxx \
+    --with-cairo \
+    --with-cairo-includes=$LIBS/include/cairo \
+    --with-cairo-libs=$LIBS/lib \
+    --with-cairo-ldflags="-lcairo" \
+    --with-zstd \
+    --with-zstd-libs=$LIBS/lib \
+    --with-zstd-includes=$LIBS/include \
+    --with-bzlib \
+    --with-bzlib-libs=$LIBS/lib \
+    --with-bzlib-includes=$LIBS/include \
+    --with-netcdf=$LIBS/bin/nc-config \
+    --with-blas \
+    --with-blas-libs=$LIBS/lib \
+    --with-blas-includes=$LIBS/include \
+    --with-lapack \
+    --with-lapack-includes=$LIBS/include \
+    --with-lapack-libs=$LIBS/lib \
+    --with-netcdf=$LIBS/bin/nc-config \
+    --with-nls \
+    --with-libs=$LIBS/lib \
+    --with-includes=$LIBS/include \
+    --with-pdal=$LIBS/bin/pdal-config \
+    --with-pdal-libs=$LIBS/lib \
+    --with-readline \
+    --with-readline-includes=$LIBS/include/readline \
+    --with-readline-libs=$LIBS/lib
+
+# Compile and install
 
 make
 make install
 
-# additional runtime dependencies
-# conda create --prefix bin/conda --clone /usr/local/apps/miniconda
-# conda install --prefix bin/conda numpy
 # for scripts:
 # eval "$(conda shell.bash hook)"
 # conda activate bin/conda
