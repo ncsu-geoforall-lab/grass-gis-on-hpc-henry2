d12dddac3edf526ff6b03ca39582f2a6bb856a30
Vaclav Petras <wenzeslaus@gmail.com>
2021-06-23 10:19:48 -0400

    Parametrize data handling in tests (#15)
    * Parametrize data dir for tests
    * Rename tests to use underscore everywhere not dash
    * Add data dir to CI

--- test_thorough.sh
+++ test_thorough.sh
@@ -2,22 +2,23 @@
 
 set -o errexit
 
-if [[ $# -ne 2 ]]; then
-    echo >&2 "Usage: $0 GRASS_COMMAND GRASS_SOURCE_CODE"
+if [[ $# -ne 3 ]]; then
+    echo >&2 "Usage: $0 GRASS_COMMAND GRASS_SOURCE_CODE DATA_DIR"
     exit 1
 fi
 
 GRASS_COMMAND="$1"
 GRASS_SOURCE_CODE="$2"
+DATABASE="$3"
 
 "$GRASS_COMMAND" --tmp-location XY --exec \
     g.extension g.download.location
 "$GRASS_COMMAND" --tmp-location XY --exec \
-    g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase="$HOME"
+    g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase="$DATABASE"
 
 cd "$GRASS_SOURCE_CODE"
 
 "$GRASS_COMMAND" --tmp-location XY --exec \
     python3 -m grass.gunittest.main \
-    --grassdata "$HOME" --location nc_spm_full_v2alpha2 --location-type nc \
+    --grassdata "$DATABASE" --location nc_spm_full_v2alpha2 --location-type nc \
     --min-success 50
