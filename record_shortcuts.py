4e1417bcdcf5a22fdbe8490149e6c6baee1e64cd
Vaclav Petras <wenzeslaus@gmail.com>
2021-08-03 11:43:10 -0400

    Black, Flake8, Pylint

--- record_shortcuts.py
+++ record_shortcuts.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-import sys
+"""Record default module version and symlinks as shortcuts"""
+
 import os
-from pathlib import Path
 import re
+import sys
 from datetime import datetime
+from pathlib import Path
+
 
 def read_and_record_shortcuts(path, filename):
+    """Read content of the directory, check validity, record to YAML file"""
     path = Path(path)
     symlinks = sorted([x for x in path.iterdir() if x.is_symlink()])
 
@@ -14,35 +18,46 @@ def read_and_record_shortcuts(path, filename):
     version_file = path / ".version"
     if version_file.exists():
         version_content = version_file.read_text()
-        match = re.search("^set ModulesVersion (.+)$", version_content, flags=re.MULTILINE)
+        match = re.search(
+            "^set ModulesVersion (.+)$", version_content, flags=re.MULTILINE
+        )
         if match:
             default_version = match.group(1)
         else:
-            exit("Module .version file exists, but ModulesVersion seems to be missing")
+            sys.exit(
+                "Module .version file exists, but ModulesVersion seems to be missing"
+            )
         if not (path / default_version).exists():
-            exit(f"Module .version file exists, but version '{default_version}' does not")
+            sys.exit(
+                f"Module .version file exists, but version '{default_version}' does not"
+            )
 
     with open(filename, "w") as record:
-        date = datetime.today().strftime('%Y-%m-%d')
+        date = datetime.today().strftime("%Y-%m-%d")
         record.write(f"date: {date}\n")
         if default_version:
             record.write(f"default: {default_version}\n")
         if symlinks:
-            record.write(f"symbolic_links:\n")
+            record.write("symbolic_links:\n")
             for symlink in symlinks:
                 target = os.readlink(symlink)
                 if not symlink.exists():
-                    exit(f"Symlink '{symlink}' exists, but the target '{target}' does not")
+                    sys.exit(
+                        f"Symlink '{symlink}' exists,"
+                        f" but the target '{target}' does not"
+                    )
                 record.write(f"  - name: {symlink.name}\n")
                 record.write(f"    points_to: {target}\n")
 
 
 def main():
+    """Check parameters, make sure output directory exists, record shortcuts"""
     if len(sys.argv) != 2:
-        exit(f"Usage: {sys.argv[0]} MODULE_DIR")
+        sys.exit(f"Usage: {sys.argv[0]} MODULE_DIR")
     metadata_dir = Path("available")
     metadata_dir.mkdir(exist_ok=True)
     read_and_record_shortcuts(sys.argv[1], metadata_dir / "shortcuts.yml")
+
 
 if __name__ == "__main__":
     main()
