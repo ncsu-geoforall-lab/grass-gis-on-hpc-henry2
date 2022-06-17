6bd7c297619113ef5712b27c128b32a39e490c13
Vaclav Petras <wenzeslaus@gmail.com>
2022-06-17 15:08:41 -0400

    Copy from cache instead of download for the test to run on scratch space offline

--- test_thorough.sh
+++ test_thorough.sh
@@ -2,14 +2,15 @@
 
 set -o errexit
 
-if [[ $# -ne 3 ]]; then
-    echo >&2 "Usage: $0 GRASS_COMMAND GRASS_SOURCE_CODE DATA_DIR"
+if [[ $# -ne 4 ]]; then
+    echo >&2 "Usage: $0 GRASS_COMMAND GRASS_SOURCE_CODE DATA_DIR DATA_CACHE"
     exit 1
 fi
 
 GRASS_COMMAND="$1"
 GRASS_SOURCE_CODE="$2"
 DATABASE="$3"
+CACHE="$4"
 
 DATABASE=$(realpath -s "$DATABASE")
 
@@ -19,7 +20,9 @@ if ! "$GRASS_COMMAND" --tmp-location XY --exec g.download.location --help; then
         g.extension g.download.location
 fi
 # Download only if the directory does not exist.
-if [ ! -d "$DATABASE/nc_spm_full_v2alpha2" ]; then
+if [ -d "$CACHE/nc_spm_full_v2alpha2" ]; then
+    cp -r "$CACHE/nc_spm_full_v2alpha2" "$DATABASE"
+else
     "$GRASS_COMMAND" --tmp-location XY --exec \
         g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path="$DATABASE"
 fi
@@ -29,4 +32,5 @@ cd "$GRASS_SOURCE_CODE"
 "$GRASS_COMMAND" --tmp-location XY --exec \
     python3 -m grass.gunittest.main \
     --grassdata "$DATABASE" --location nc_spm_full_v2alpha2 --location-type nc \
-    --min-success 50
+    --min-success 50 \
+    --output "$DATABASE/testreport-$(basename $GRASS_SOURCE_CODE)"
